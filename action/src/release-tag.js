1a3f286af1976e65cd9030c7e12d692706d7a98c
heibaikn <heibaikn@163.com>
2023-11-04 10:10:13 +0800

    Squashed commit of test workflow

--- action/src/release-tag.js
+++ action/src/release-tag.js
@@ -6,18 +6,52 @@ async function run() {
   // The YML workflow will need to set myToken with the GitHub Secret Token
   // myToken: ${{ secrets.GITHUB_TOKEN }}
   // https://help.github.com/en/actions/automating-your-workflow-with-github-actions/authenticating-with-the-github_token#about-the-github_token-secret
-  const myToken = core.getInput('myToken');
-
-  const octokit = github.getOctokit(myToken)
+  const myToken = core.getInput('GITHUB_TOKEN');
+  console.log(myToken);
 
   // You can also pass in additional options as a second parameter to getOctokit
   // const octokit = github.getOctokit(myToken, {userAgent: "MyActionVersion1"});
 
-  await octokit.rest.git.createTag({
-    owner,
-    repo,
-    tag
-  })
+  // Get owner and repo from context of payload that triggered the action
+  const { owner, repo } = context.repo
+
+  // Get the inputs from the workflow file: https://github.com/actions/toolkit/tree/master/packages/core#inputsoutputs
+  const tagName = core.getInput('VERSION', { required: true })
+
+  // This removes the 'refs/tags' portion of the string, i.e. from 'refs/tags/v1.10.15' to 'v1.10.15'
+  const tag = tagName.replace('refs/tags/', '')
+  const releaseName =
+    core.getInput('release_name', { required: false }) || tag
+  const body = core.getInput('body', { required: false }) || ''
+  const draft = core.getInput('draft', { required: false }) === 'true'
+  const prerelease = /\d-[a-z]/.test(tag)
+  
+  const commitSha = process.env.GITHUB_SHA;
+  
+  const octokit = github.getOctokit(myToken)
+
+  const tagResponse = await octokit.rest.git.createTag({
+    owner: owner,
+    repo: repo,
+    tag: tagName,
+    message: "",
+    object: commitSha,
+    type: 'commit',
+    tagger: {
+      name: "",
+      email: ""
+    }
+  });
+
+  //   octokit.rest.git.createTag({
+  //     owner,
+  // repo,
+  // tag,
+  // message,
+  // object,
+  // type,
+
+  //   })
   // const { data: pullRequest } = await octokit.rest.pulls.get({
   //     owner: 'octokit',
   //     repo: 'rest.js',
