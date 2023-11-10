74564ab82c93754206c6655785a402a5c963513f
heibaikn <heibaikn@163.com>
2023-11-10 21:36:04 +0800

    feat: rm code

--- scripts/release.js
+++ scripts/release.js
@@ -30,58 +30,44 @@ const skipBuild = args.skipBuild
 const isCanary = args.canary
 const skipPrompts = args.skipPrompts || args.canary
 const skipGit = args.skipGit || args.canary
-const updateTypeArr = ["major", "premajor", "minor", "preminor", "patch", "prepatch", "prerelease"]
+const updateTypeArr = ['major', 'premajor', 'minor', 'preminor', 'patch', 'prepatch', 'prerelease']
 
-const updateType = updateTypeArr.filter(type => {
-  return args[type] && type
-}).shift()
-console.log(updateType);
+const updateType = updateTypeArr
+  .filter((type) => {
+    return args[type] && type
+  })
+  .shift()
+console.log(updateType)
 const packages = fs
   .readdirSync(path.resolve(__dirname, '../packages'))
-  .filter(p => !p.endsWith('.ts') && !p.startsWith('.'))
+  .filter((p) => !p.endsWith('.ts') && !p.startsWith('.'))
 const proPackages = fs
   .readdirSync(path.resolve(__dirname, '../www'))
-  .filter(p => !p.endsWith('.ts') && !p.startsWith('.'))
-const getPkgRoot = pkg => path.resolve(__dirname, '../packages/' + pkg)
-const getProRoot = pkg => path.resolve(__dirname, '../www/' + pkg)
+  .filter((p) => !p.endsWith('.ts') && !p.startsWith('.'))
+const getPkgRoot = (pkg) => path.resolve(__dirname, '../packages/' + pkg)
+const getProRoot = (pkg) => path.resolve(__dirname, '../www/' + pkg)
 
-const isCorePackage = pkgName => {
+const isCorePackage = (pkgName) => {
   if (!pkgName) return
 
   if (pkgName === 'vue' || pkgName === '@vue/compat') {
     return true
   }
 
-  return (
-    pkgName.startsWith('@vue') &&
-    packages.includes(pkgName.replace(/^@vue\//, ''))
-  )
+  return pkgName.startsWith('@vue') && packages.includes(pkgName.replace(/^@vue\//, ''))
 }
 
-const renamePackageToCanary = pkgName => {
-  if (pkgName === 'vue') {
-    return '@vue/canary'
-  }
-
-  if (isCorePackage(pkgName)) {
-    return `${pkgName}-canary`
-  }
-
-  return pkgName
-}
-
-const keepThePackageName = pkgName => pkgName
+const keepThePackageName = (pkgName) => pkgName
 
 const skippedPackages = []
 
-const inc = i => semver.inc(currentVersion, i, preId)
-const run = (bin, args, opts = {}) =>
-  execa(bin, args, { stdio: 'inherit', ...opts })
+const inc = (i) => semver.inc(currentVersion, i, preId)
+const run = (bin, args, opts = {}) => execa(bin, args, { stdio: 'inherit', ...opts })
 const dryRun = (bin, args, opts = {}) =>
   console.log(pico.blue(`[dryrun] ${bin} ${args.join(' ')}`), opts)
 const runIfNotDry = isDryRun ? dryRun : run
 
-const step = msg => console.log(pico.cyan(msg))
+const step = (msg) => console.log(pico.cyan(msg))
 
 async function main() {
   if (!(await isInSyncWithRemote())) {
@@ -93,7 +79,7 @@ async function main() {
   let targetVersion = args._[0]
   if (updateType) {
     // @ts-ignore
-    const newVersion = semver.inc(currentVersion, updateType);
+    const newVersion = semver.inc(currentVersion, updateType)
     // @ts-ignore
     targetVersion = newVersion
   } else if (isCanary) {
@@ -110,18 +96,12 @@ async function main() {
   }
 
   step(
-    isCanary
-      ? `Releasing canary version v${targetVersion}...`
-      : `Releasing v${targetVersion}...`
+    isCanary ? `Releasing canary version v${targetVersion}...` : `Releasing v${targetVersion}...`
   )
-
 
   // update all package versions and inter-dependencies
   step('\nUpdating cross dependencies...')
-  updateVersions(
-    targetVersion,
-    isCanary ? keepThePackageName : keepThePackageName
-  )
+  updateVersions(targetVersion, isCanary ? keepThePackageName : keepThePackageName)
   versionUpdated = true
 
   // build all packages with types
@@ -148,28 +128,20 @@ async function main() {
     await runIfNotDry('git', ['tag', `v${targetVersion}`])
     await runIfNotDry('git', ['push', 'origin', `refs/tags/v${targetVersion}`])
     await runIfNotDry('git', ['push'])
-
   } else {
     console.log('No changes to commit.')
   }
-
-
 }
-
 
 async function isInSyncWithRemote() {
   try {
     const repoName = await getRepoName()
     const branch = await getBranch()
-    const res = await fetch(
-      `https://api.github.com/repos/${repoName}/commits/${branch}?per_page=1`
-    )
+    const res = await fetch(`https://api.github.com/repos/${repoName}/commits/${branch}?per_page=1`)
     const data = await res.json()
     return data.sha === (await getSha())
   } catch (e) {
-    console.error(
-      'Failed to check whether local HEAD is up-to-date with remote.'
-    )
+    console.error('Failed to check whether local HEAD is up-to-date with remote.')
     return false
   }
 }
@@ -198,13 +170,9 @@ function updateVersions(version, getNewPackageName = keepThePackageName) {
   // 1. update root package.json
   updatePackage(path.resolve(__dirname, '..'), version, getNewPackageName)
   // 2. update all packages
-  packages.forEach(p =>
-    updatePackage(getPkgRoot(p), version, getNewPackageName)
-  )
+  packages.forEach((p) => updatePackage(getPkgRoot(p), version, getNewPackageName))
   // 3. update all project
-  proPackages.forEach(p =>
-    updatePackage(getProRoot(p), version, getNewPackageName)
-  )
+  proPackages.forEach((p) => updatePackage(getProRoot(p), version, getNewPackageName))
 }
 
 function updatePackage(pkgRoot, version, getNewPackageName) {
@@ -220,16 +188,14 @@ function updatePackage(pkgRoot, version, getNewPackageName) {
 function updateDeps(pkg, depType, version, getNewPackageName) {
   const deps = pkg[depType]
   if (!deps) return
-  Object.keys(deps).forEach(dep => {
+  Object.keys(deps).forEach((dep) => {
     if (deps[dep] === 'workspace:*') {
       return
     }
     if (isCorePackage(dep)) {
       const newName = getNewPackageName(dep)
       const newVersion = newName === dep ? version : `npm:${newName}@${version}`
-      console.log(
-        pico.yellow(`${pkg.name} -> ${depType} -> ${dep}@${newVersion}`)
-      )
+      console.log(pico.yellow(`${pkg.name} -> ${depType} -> ${dep}@${newVersion}`))
       deps[dep] = newVersion
     }
   })
@@ -283,7 +249,7 @@ async function publishPackage(pkgName, version, additionalFlags) {
   }
 }
 
-main().catch(err => {
+main().catch((err) => {
   if (versionUpdated) {
     // revert to current version on failed releases
     updateVersions(currentVersion)
